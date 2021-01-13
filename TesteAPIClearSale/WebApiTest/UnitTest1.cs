caa2c837bef3bf7c97dd38bb2f5fe2f512c15954
MathPalma <matheus.palma692@gmail.com>
2021-01-13 18:10:24 -0300

    Fixing git ignore

---
+++ TesteAPIClearSale/WebApiTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using TesteAPIClearSale.Models;
+
+namespace WebApiTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Person a;
+        }
+    }
+}
